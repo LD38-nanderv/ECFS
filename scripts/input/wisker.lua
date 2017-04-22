b04b6795915643d69bf5d6b956cfad570f1a1ec1
nanderv <nanderv@gmail.com>
2017-04-22 11:04:54 +0200

    asdf

--- scripts/input/wisker.lua
+++ scripts/input/wisker.lua
@@ -1,16 +1,21 @@
 return function(dt)
-	COLS = {}
-	for k,v in pairs(E.wiskers) do
-		local sx = v.position.x
-		local sy = v.position.y
-		love.graphics.circle("line",v.position.x, v.position.y, 10)
-		local a = {}
-		for l,w in ipairs(v.wiskers) do
-			local aa, b, c = scripts.systems.collision.init.ray(v, {x=0,y=0}, {x=w.x, y=w.y})
-			a[w] = {entity=a, x=b, y=c}
-		end
+    for _, v in pairs(E.wiskers) do
 
-		core.add_event("pre",scripts.events.create_wisker_collision_event(v, a))
-	end
-	
+        love.graphics.circle("line", v.position.x, v.position.y, 10)
+        local a = {}
+        local beep = false
+        for l, w in ipairs(v.wiskers) do
+            local aa, b, c = scripts.systems.collision.init.ray(v, { x = 0, y = 0 }, { x = w.x, y = w.y })
+
+            if aa then
+                a[w] = { entity = a, x = b, y = c }
+                local d, e = b - v.position.x, c - v.position.y
+                print(d, e, math.sqrt(d * d + e * e))
+                beep = true
+            end
+        end
+        if beep then
+            core.add_event("pre", scripts.events.create_wisker_collision_event(v, a))
+        end
+    end
 end
